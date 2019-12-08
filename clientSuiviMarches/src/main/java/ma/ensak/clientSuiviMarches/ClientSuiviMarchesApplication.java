c044837dec9281699d9001f2e5aa089637e188e5
ytahi <yacinetahi@gmail.com>
2019-12-08 18:48:56 +0100

    partie client

---
+++ clientSuiviMarches/src/main/java/ma/ensak/clientSuiviMarches/ClientSuiviMarchesApplication.java
@@ -0,0 +1,13 @@
+package ma.ensak.clientSuiviMarches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSuiviMarchesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSuiviMarchesApplication.class, args);
+	}
+
+}
---
+++ clientSuiviMarches/src/test/java/ma/ensak/clientSuiviMarches/ClientSuiviMarchesApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensak.clientSuiviMarches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSuiviMarchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
