5d3106aed3dc8e0fb679f98fa057c79e6060269c
ytahi <yacinetahi@gmail.com>
2020-01-14 17:17:17 +0100

    la fin

--- microservice-projet/src/main/java/ensak/suivi_marche/Microservice_projet/MicroserviceProjectApplication.java
+++ microservice-projet/src/main/java/ensak/suivi_marche/Microservice_projet/MicroserviceProjectApplication.java
@@ -3,7 +3,10 @@ package ensak.suivi_marche.Microservice_projet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MicroserviceProjectApplication {
 
 	public static void main(String[] args) {
